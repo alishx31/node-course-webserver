49993c2e82174af3a19659af55b9307cbd02f17e
alishx31 <alexmagar42@gmail.com>
2018-06-06 23:57:49 +0545

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const hbs=require('hbs');
 
+const port= process.env.PORT || 3000;
 var app =express();
 
 app.set('view engine','hbs');
@@ -56,6 +57,6 @@ errorMessage:'unable to handle req'
 
 
 
-app.listen(3000, ()=>{
-    console.log('server is up in 3000');
+app.listen(port, ()=>{
+    console.log(`server is up in ${port}`);
 });
